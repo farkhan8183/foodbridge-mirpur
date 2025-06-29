6619da42e38a5678a8327ff3856f20993441ee9e
Abdul Haadi <abdulhaadi@Abduls-MacBook-Air-2.local>
2025-06-29 10:17:39 +0500

    Started the php backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import About from "./pages/About";
 import Mission from "./pages/Mission";
 import Contact from "./pages/Contact";
 import Login from "./pages/donor/DonorLogin";
+import DonorPanel from "./pages/donor/DonorPanel"; // Add this import
 import Reclogin from "./pages/Recepient/RecipientLogin";
-import  Vollogin  from "./pages/volunteer/volunteer"
+import Vollogin from "./pages/volunteer/volunteer";
+
 function MainLayout() {   // this will always show navbar at top 
   return (
     <>
@@ -28,10 +30,11 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Route>
 
-        {/* These pages won't show Navbar */}
+        {/* These pages won't show Navbar (login pages and dashboards) */}
         <Route path="/donorlogin" element={<Login />} />
+        <Route path="/donorpanel" element={<DonorPanel />} />
         <Route path="/reclogin" element={<Reclogin />} />
-          <Route path="/vollogin" element={<Vollogin />} />
+        <Route path="/vollogin" element={<Vollogin />} />
       </Routes>
     </Router>
   );
